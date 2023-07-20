43bc64d7ac96090fed676440532ecaf9cc4547b9
Abuibrahimbbg <abuibrahimbbg@gmail.com>
2023-07-20 07:24:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int _isupper(int c) {
+    // Check if the character is an uppercase letter based on ASCII values
+    if (c >= 'A' && c <= 'Z') {
+        return 1; // Return 1 if the character is uppercase
+    } else {
+        return 0; // Return 0 if the character is not uppercase
+    }
+}
